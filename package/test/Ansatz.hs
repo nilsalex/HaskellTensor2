489e3b5ec8ee5003ea2470e29023b8ab158bb3ab
Nils Alex <nils@famalex.de>
2020-07-20 13:34:21 +0200

    ghc version bump, tests

--- package/test/Ansatz.hs
+++ package/test/Ansatz.hs
@@ -11,7 +11,7 @@ import Test.Tasty.HUnit
 ansatzForestAIEpsilon :: Bool
 ansatzForestAIEpsilon = actual == expected
   where
-    (_,eps6,_) = mkAnsatzTensorFastAbs 6 symList6 areaList6 :: (AnsatzForestEta, AnsatzForestEpsilon, ATens 1 0 1 0 0 0 AnsVarR)
+    (_,eps6,_) = mkAnsatzTensorFastAbs 6 symList6 areaList6 :: (AnsatzForestEta, AnsatzForestEpsilon, ATens 0 1 0 1 0 0 AnsVarR)
     actual = drawAnsatzEpsilon eps6
     expected = "(1,2,3,4)\n|\n`---- (5,6) * (16) * x[3]\n\n"
 
@@ -22,7 +22,7 @@ ansatz14_1Fast :: Bool
 ansatz14_1Fast = rank == 110
   where
     evalL = map (\(x,_,_) -> x) areaList14_1
-    (_,_,ans) = mkAnsatzTensorFast 14 symList14_1 evalL :: (AnsatzForestEta, AnsatzForestEpsilon, STTens 14 0 AnsVarR)
+    (_,_,ans) = mkAnsatzTensorFast 14 symList14_1 evalL :: (AnsatzForestEta, AnsatzForestEpsilon, STTens 0 14 AnsVarR)
     rank = tensorRank2' ans
 
 testCase2 = testCase "ABpCq ansatz rank" $
@@ -32,7 +32,7 @@ ansatz14_1Incr :: Bool
 ansatz14_1Incr = rank == 110
   where
     evalL = map (\(x,_,_) -> x) areaList14_1
-    (_,_,ans) = mkAnsatzTensorIncremental 14 symList14_1 evalL :: (AnsatzForestEta, AnsatzForestEpsilon, STTens 14 0 AnsVarR)
+    (_,_,ans) = mkAnsatzTensorIncremental 14 symList14_1 evalL :: (AnsatzForestEta, AnsatzForestEpsilon, STTens 0 14 AnsVarR)
     rank = tensorRank2' ans
 
 testCase3 = testCase "ABpCq ansatz (incremental) rank" $
@@ -41,7 +41,7 @@ testCase3 = testCase "ABpCq ansatz (incremental) rank" $
 ansatz14_2Fast :: Bool
 ansatz14_2Fast = rank == 72
   where
-    (_,_,ans) = mkAnsatzTensorFastAbs 14 symList14_2 areaList14_2 :: (AnsatzForestEta, AnsatzForestEpsilon, ATens 3 0 1 0 0 0 AnsVarR)
+    (_,_,ans) = mkAnsatzTensorFastAbs 14 symList14_2 areaList14_2 :: (AnsatzForestEta, AnsatzForestEpsilon, ATens 0 3 0 1 0 0 AnsVarR)
     rank = tensorRank6' ans
 
 testCase4 = testCase "ABCI ansatz rank" $
@@ -50,7 +50,7 @@ testCase4 = testCase "ABCI ansatz rank" $
 ansatz14_2Incr :: Bool
 ansatz14_2Incr = rank == 72
   where
-    (_,_,ans) = mkAnsatzTensorIncrementalAbs 14 symList14_2 areaList14_2 :: (AnsatzForestEta, AnsatzForestEpsilon, ATens 3 0 1 0 0 0 AnsVarR)
+    (_,_,ans) = mkAnsatzTensorIncrementalAbs 14 symList14_2 areaList14_2 :: (AnsatzForestEta, AnsatzForestEpsilon, ATens 0 3 0 1 0 0 AnsVarR)
     rank = tensorRank6' ans
 
 testCase5 = testCase "ABCI ansatz (incremental) rank" $
